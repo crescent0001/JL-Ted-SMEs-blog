a558a4b47b78674d248a6f1f87908f0bd0ae670f
JianyiLii <96557600+JianyiLii@users.noreply.github.com>
2021-12-26 21:16:57 +0800

    update

--- Demo Project/JannetLee/views.py
+++ Demo Project/JannetLee/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.core.mail import send_mail
+from django.conf import settings
 
 def home(request):
     return render(request, 'contactus.html', {})
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        Enquiry = request.POST['Enquiry']
+        message = request.POST['message']
+
+        # sned an email 
+        send_mail(
+            'name', # subject 
+            'message', # message 
+            'email', # from email 
+            ['jianyi2021@gmail.com'], # to Email
+        )
+
+        return render(request, 'contactus.html', {'name' : name })
+
+    else:
+        return render(request, 'contactus.html', {})
     # contact = Post.objects.all()
     # return render(request, 'template/aboutus.html', {'aboutus' : aboutus })
-    return render(request, 'contactus.html', {})
+    
 
 
 def searchresults(request): #https://www.youtube.com/watch?v=AGtae4L5BbI, to be continued (09:44)
--- Demo Project/TSA/settings.py
+++ Demo Project/TSA/settings.py
@@ -123,3 +123,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# myaccout.google.com/lesssecureapps
+# myaccout.google.com/DisplayUnlockCaptcha
+# myaccout.google.com/apppasswords
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackens'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'jianyi2021@gmail.com'
+EMAIL_HOST_PASSWORD = 'mkqxcidtgqyruhtl'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
