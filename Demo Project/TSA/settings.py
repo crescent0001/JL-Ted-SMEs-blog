2ccabc3a2c2674ec716649a8e0ec6f4ab62ad35a
JianyiLii <96557600+JianyiLii@users.noreply.github.com>
2021-12-27 13:19:32 +0800

    update of Enquiry

--- Demo Project/TSA/settings.py
+++ Demo Project/TSA/settings.py
@@ -132,7 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jianyi2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'mkqxcidtgqyruhtl'
+EMAIL_HOST_PASSWORD = 'kocBa4-qypbud-hujrij'
 EMAIL_USE_TLS = True 
 EMAIL_USE_SSL = False
 
