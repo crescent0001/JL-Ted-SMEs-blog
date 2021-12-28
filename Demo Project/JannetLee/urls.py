4fb35e2adecaa803d7812189c7c570a6989da1a2
Winston Masoen <winstonvm060705@gmail.com>
2021-12-28 14:04:07 +0700

    Null

--- Demo Project/JannetLee/urls.py
+++ Demo Project/JannetLee/urls.py
@@ -3,9 +3,9 @@ from . import views
 # from .views import HomeView
 
 urlpatterns = [
+    # path('', HomeView.as_view(), name='home'),
     path('', views.home, name="home"),
     path('searchresults', views.searchresults, name='searchresults'),
-    # path('', HomeView.as_view(), name='home'),
     path('contactus.html', views.contact, name="contactus"),
     
 ]
