4b6a1dd187bfd107a639c23ec9425492275c1870
JianyiLii <96557600+JianyiLii@users.noreply.github.com>
2021-12-28 11:36:01 +0800

    Done with Enquiry
    and i have added a MyUser for recording the Enquiry user.

--- Demo Project/JannetLee/admin.py
+++ Demo Project/JannetLee/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Category, Enquiry
+from .models import Post, Category, Enquiry, MyUser
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Enquiry)
+admin.site.register(MyUser)
