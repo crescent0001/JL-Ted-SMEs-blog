a87eea3a4f0796a3def345e70de6214870dc4bb4
Wolooloo <93443273+Wolooloo@users.noreply.github.com>
2021-12-29 23:25:34 +0700

    Fixed Pages link

--- Demo Project/JannetLee/urls.py
+++ Demo Project/JannetLee/urls.py
@@ -4,12 +4,12 @@ from .views import HomeView, ArticlePage, all_contact, IndexView, AboutPageView,
 
 urlpatterns = [
     
-    path('', IndexView.as_view(), name="home"),
+    path('index.html', views.IndexView.as_view(), name="Home"),
     # path('searchresults', views.searchresults, name='searchresults'),
     path('about.html', views.AboutPageView.as_view(), name='About Us'),
     path('bestpractices.html', views.HomeView.as_view(), name='Best Practices'),
     path('futureupdates.html', views.FutureUpdates.as_view(), name='Future Updates'),
-    # path('contactuc.html', views.Contact, name="contactus"),
+    # path('contactus.html', views.Contact, name="contactus"),
     path('article/<int:pk>', ArticlePage.as_view(), name='articlepage')
 
 ]
