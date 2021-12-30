97a2b068589b015d5aa08198e546debd480b0bd7
ddevarsh7 <ddevarsh7@gmail.com>
2021-12-30 08:42:33 +0530

    DYNAMIC IMAGES ARE NOW WORKING!!!!!!!!!

--- Demo Project/JannetLee/urls.py
+++ Demo Project/JannetLee/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from .views import HomeView, ArticlePage, all_contact, IndexView, AboutPageView, FutureUpdates, contactupdate, Homepage
 
@@ -11,7 +13,10 @@ urlpatterns = [
     path('futureupdates.html', views.FutureUpdates.as_view(), name='Future Updates'),
     # path('contactuc.html', views.Contact, name="contactus"),
     path('article/<int:pk>', ArticlePage.as_view(), name='articlepage'),
-    path('contactus.html', views.contactupdate.as_view(), name='Contact Us'),
     path('index.html', views.Homepage.as_view(), name='Contact Us'),
 
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
